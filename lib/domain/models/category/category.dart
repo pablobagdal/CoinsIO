1bb15597973067a507b9ae1d0aa9b8f9afd53dcb
pablobagdal <pablobagdal@gmail.com>
2025-06-10 01:13:56 +0300

    add category

---
+++ lib/domain/models/category/category.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'category.freezed.dart';
+part 'category.g.dart';
+
+@freezed
+abstract class Category with _$Category {
+  const factory Category({
+    // example: 1
+    required int id,
+    // example: Зарплата
+    required String name,
+    // example: 💰
+    required String emoji,
+    // example: true
+    required bool isIncome,
+  }) = _Category;
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      _$CategoryFromJson(json);
+}
