8c109b0f23fbb649ec7eed5b50fded34f8ffa859
pablobagdal <pablobagdal@gmail.com>
2025-06-10 01:17:14 +0300

    add transaction

---
+++ lib/domain/models/transaction/transaction.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'transaction.freezed.dart';
+part 'transaction.g.dart';
+
+@freezed
+abstract class Transaction with _$Transaction {
+  const factory Transaction({
+    // example: 1
+    required int id,
+    // example: 1
+    required int accountId,
+    // example: 1
+    required int categoryId,
+    // example: 500.00
+    required String amount,
+    // example: 2023-10-01T12:00:00Z
+    required DateTime transactionDate,
+    // example: Зарплата за месяц
+    String? comment,
+    // example: 2023-10-01T12:00:00Z
+    required DateTime createdAt,
+    // example: 2023-10-01T12:00:00Z
+    required DateTime updatedAt,
+  }) = _Transaction;
+
+  factory Transaction.fromJson(Map<String, dynamic> json) =>
+      _$TransactionFromJson(json);
+}
