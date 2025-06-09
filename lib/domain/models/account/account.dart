bb680cb6235f705c3f6760c645f9061359e5c74c
pablobagdal <pablobagdal@gmail.com>
2025-06-10 00:31:04 +0300

    add abstract keyword to account class

--- lib/domain/models/account/account.dart
+++ lib/domain/models/account/account.dart
@@ -4,7 +4,7 @@ part 'account.freezed.dart';
 part 'account.g.dart';
 
 @freezed
-class Account with _$Account {
+abstract class Account with _$Account {
   const factory Account({
     // example: 1
     required int id,
