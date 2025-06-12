898dc097393328d671e8e4726dc8c58aaef7d14b
pablobagdal <pablobagdal@gmail.com>
2025-06-12 14:19:01 +0300

    add setState to control ephemeral state of navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -39,21 +39,63 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Главная'),
-          NavigationDestination(
-            icon: Icon(Icons.account_balance),
-            label: 'Счета',
+      bottomNavigationBar: MyNavigationBar(),
+    );
+  }
+}
+
+class MyNavigationBar extends StatefulWidget {
+  const MyNavigationBar({super.key});
+
+  @override
+  State<MyNavigationBar> createState() => _MyNavigationBarState();
+}
+
+class _MyNavigationBarState extends State<MyNavigationBar> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: const [
+        NavigationDestination(
+          icon: ImageIcon(
+            AssetImage('assets/navigation_bar_icons/spendings_icon.png'),
           ),
-          NavigationDestination(icon: Icon(Icons.settings), label: 'Настройки'),
-        ],
-        selectedIndex: 0,
-        onDestinationSelected: (value) {
-          // обработчик нажатия пока пустой
-          // Здесь можно добавить логику для переключения между экранами
-        },
-      ),
+          label: 'Расходы',
+        ),
+        NavigationDestination(
+          icon: ImageIcon(
+            AssetImage('assets/navigation_bar_icons/incomes_icon.png'),
+          ),
+          label: 'Доходы',
+        ),
+        NavigationDestination(
+          icon: ImageIcon(
+            AssetImage('assets/navigation_bar_icons/accounts_icon.png'),
+          ),
+          label: 'Счёт',
+        ),
+        NavigationDestination(
+          icon: ImageIcon(
+            AssetImage('assets/navigation_bar_icons/categories_icon.png'),
+          ),
+          label: 'Статьи',
+        ),
+        NavigationDestination(
+          icon: ImageIcon(
+            AssetImage('assets/navigation_bar_icons/settings_icon.png'),
+          ),
+          label: 'Настройки',
+        ),
+      ],
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (value) {
+        // Здесь можно добавить логику для переключения между экранами
+        setState(() {
+          selectedIndex = value;
+        });
+      },
     );
   }
 }
