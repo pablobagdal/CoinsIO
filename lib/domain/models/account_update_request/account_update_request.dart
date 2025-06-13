a5fb304c4e2bd0b18144ba3c2be3f96030144c43
pablobagdal <pablobagdal@gmail.com>
2025-06-13 18:30:42 +0300

    add and generate account_update_request

---
+++ lib/domain/models/account_update_request/account_update_request.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'account_update_request.freezed.dart';
+part 'account_update_request.g.dart';
+
+@freezed
+abstract class AccountUpdateRequest with _$AccountUpdateRequest {
+  const factory AccountUpdateRequest({
+    // example: Основной счёт
+    required String name,
+    // example: 100.00
+    required String balance,
+    // example: RUB
+    required String currency,
+  }) = _AccountUpdateRequest;
+
+  factory AccountUpdateRequest.fromJson(Map<String, dynamic> json) =>
+      _$AccountUpdateRequestFromJson(json);
+}
