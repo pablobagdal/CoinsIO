42a472355bbaca6df36d651152d0c5a885510577
pablobagdal <pablobagdal@gmail.com>
2025-06-13 17:43:20 +0300

    change pathes to assets

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,7 +1,12 @@
 abstract class Assets {
-  static const String iconSpendings = 'assets/icons/spendings.png';
-  static const String iconIncomes = 'assets/icons/incomes.png';
-  static const String iconAccounts = 'assets/icons/accounts.png';
-  static const String iconCategories = 'assets/icons/categories.png';
-  static const String iconSettings = 'assets/icons/settings.png';
+  static const String iconSpendings =
+      'assets/navigation_bar_icons/spendings_icon.png';
+  static const String iconIncomes =
+      'assets/navigation_bar_icons/incomes_icon.png';
+  static const String iconAccounts =
+      'assets/navigation_bar_icons/accounts_icon.png';
+  static const String iconCategories =
+      'assets/navigation_bar_icons/categories_icon.png';
+  static const String iconSettings =
+      'assets/navigation_bar_icons/settings_icon.png';
 }
