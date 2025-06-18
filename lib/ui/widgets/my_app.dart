1f2e99b16b706b56f8b1b7846b1055723fb66081
pablobagdal <pablobagdal@gmail.com>
2025-06-18 11:42:25 +0300

    remove useless import

--- lib/ui/widgets/my_app.dart
+++ lib/ui/widgets/my_app.dart
@@ -2,7 +2,6 @@ import 'package:coinio_app/core/themes/app_theme.dart';
 import 'package:coinio_app/main.dart';
 import 'package:coinio_app/ui/blocs/navigation_bloc/navigation_bloc.dart';
 import 'package:coinio_app/ui/blocs/transaction_bloc/transaction_bloc.dart';
-import 'package:coinio_app/ui/blocs/transaction_bloc/transaction_state.dart';
 import 'package:coinio_app/ui/widgets/my_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
