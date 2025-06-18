b69908c48843462ca2e992533c22831704fec58f
pablobagdal <pablobagdal@gmail.com>
2025-06-18 11:40:30 +0300

    add app dependencies class to manage all usecases until create a good dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'package:coinio_app/data/repositories/transaction_repository.dart';
+import 'package:coinio_app/data/services/mock_transaction_repository.dart';
+import 'package:coinio_app/domain/usecases/get_transaction_use_case.dart';
+import 'package:coinio_app/domain/usecases/get_transactions_by_period_use_case.dart';
 import 'package:coinio_app/ui/widgets/my_app.dart';
 import 'package:flutter/material.dart';
 
+/// class with usecases shared around app
+class AppDependencies {
+  late final TransactionRepository transactionRepository;
+  late final GetTransactionUseCase getTransactionUseCase;
+  late final GetTransactionsByPeriodUseCase getTransactionsByPeriodUseCase;
+
+  AppDependencies() {
+    transactionRepository = MockTransactionRepository();
+    getTransactionUseCase = GetTransactionUseCase(
+      repository: transactionRepository,
+    );
+    getTransactionsByPeriodUseCase = GetTransactionsByPeriodUseCase(
+      repository: transactionRepository,
+    );
+  }
+}
+
 void main(List<String> args) {
+  final dependencies = AppDependencies();
   // ignore: prefer_const_constructors
-  runApp(MyApp());
+  runApp(MyApp(dependencies: dependencies));
 }
--- lib/ui/widgets/my_app.dart
+++ lib/ui/widgets/my_app.dart
@@ -1,19 +1,33 @@
 import 'package:coinio_app/core/themes/app_theme.dart';
+import 'package:coinio_app/main.dart';
 import 'package:coinio_app/ui/blocs/navigation_bloc/navigation_bloc.dart';
+import 'package:coinio_app/ui/blocs/transaction_bloc/transaction_bloc.dart';
+import 'package:coinio_app/ui/blocs/transaction_bloc/transaction_state.dart';
 import 'package:coinio_app/ui/widgets/my_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  AppDependencies dependencies;
+  MyApp({super.key, required this.dependencies});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Coins IO',
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => NavigationBloc(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => NavigationBloc()),
+          BlocProvider(
+            create:
+                (context) => TransactionBloc(
+                  getTransactionUseCase: dependencies.getTransactionUseCase,
+                  getTransactionsByPeriodUseCase:
+                      dependencies.getTransactionsByPeriodUseCase,
+                ),
+          ),
+        ],
         child: const MyScreens(),
       ),
       theme: AppTheme.light,
