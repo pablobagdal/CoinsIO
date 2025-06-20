6a947352675ffed61734db08e018bb1acf6d35c6
pablobagdal <pablobagdal@gmail.com>
2025-06-20 19:43:25 +0700

    fix transaction repo according to spec

--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -1,12 +1,17 @@
 import 'package:coinio_app/domain/models/transaction/transaction.dart';
+import 'package:coinio_app/domain/models/transaction_response/transaction_response.dart';
 
 abstract class TransactionRepository {
-  Future<Transaction> getTransaction(int id);
+  Future<TransactionResponse> getTransaction(int id);
   Future<void> addTransaction(Transaction transaction);
-  Future<void> updateTransaction(Transaction transaction);
+  Future<void> updateTransaction({
+    required int id,
+    required Transaction transaction,
+  });
   Future<void> deleteTransaction(int id);
-  Future<List<Transaction>> getTransactionsByPeriod(
+  Future<List<TransactionResponse>> getTransactionsByPeriod({
+    required int id,
     DateTime startDate,
     DateTime endDate,
-  );
+  });
 }
