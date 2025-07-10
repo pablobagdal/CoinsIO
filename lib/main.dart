f62d9305da6a2bf9edd84a79e8408fe29ff03d6d
pablobagdal <pablobagdal@gmail.com>
2025-07-10 14:52:17 +0700

    add env to store key

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:coinio_app/core/utils/di.dart';
 import 'package:flutter/material.dart';
 import 'package:coinio_app/ui/widgets/my_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
   // setupDependencies(db);
   setupMockDependencies();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
 
   runApp(const MyApp());
 }
